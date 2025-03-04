dde6ebd46f03ce4cc5fbd5ba7bd7790b0d86d08c
Ajayodedara007 <ajayodedara818@gmail.com>
2025-03-05 00:21:06 +0530

    changes

--- App.js
+++ App.js
@@ -3,5 +3,3 @@
 
 const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child1' }, React.createElement('h1', { id: 'heading' }, 'Heading')))
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(parent)
